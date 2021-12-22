fc88822cc20390539a3fbeb0e40dcaf6951d4902
Steven Hastwell <stevenhastwell03@gmail.com>
2021-12-21 20:48:39 -0500

    added arrays for numbers, upper case characters, lower case characters, and special characters

--- script.js
+++ script.js
@@ -1,6 +1,93 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Assignment Code
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+var hasNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var hasLowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var hasUpperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +96,27 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword() {
+  var passwordLength = prompt("Enter the length of your password");
+  console.log(passwordLength);
+
+  var hasNumbers = confirm("Include Numbers?");
+  console.log(hasNumbers);
+
+  var specialCharacters = confirm("Include special characters?");
+  console.log(specialCharacters);
+
+  var hasUpperCase = confirm("Inculde uppercase");
+  console.log(hasUpperCase)
+
+  var hasLowerCase = confirm("Inculde lowercase");
+  console.log(hasLowerCase)
+}
+
+for( var i = 0; i < hasNumbers.length; i++)
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
